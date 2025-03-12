914274e078d65cd0f3bca3f84c281ca8d42e450f
Maria Esther <tetsguitar@gmail.com>
2025-03-12 15:03:23 -0300

    Primeira versão da estrutura do Design System, com pendência no Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: ['../src/**/*.stories.js'],  // Aponte para as histórias dentro da pasta src
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+  ],
+};
